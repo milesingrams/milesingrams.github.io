139a5f3490d6ed9472cd30f1d7b0e39a5311091d
Miles Ingram <milesingrams@gmail.com>
2019-04-16 17:28:47 -0400

    Removed unnecessary stuff

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,31 +4,98 @@
       <logo :effect="effect" color="#000000" :progress="scrollProgress"></logo>
     </div>
 
-    <color-section :color="colorAtIndex(0)">
-      <h2 class="about-text">
-        Hi, I'm Miles
-      </h2>
-    </color-section>
-    <color-section :color="colorAtIndex(1)">
-      <h2 class="about-text">
-        I Like
-      </h2>
-      <div>
-        <span class="tag">Bits</span> <span class="tag">Bots</span> <span class="tag">Bio</span> <span class="tag">Battlestar Galactica</span>
-      </div>
-    </color-section>
-    <color-section :color="colorAtIndex(2)">
-      <h2 class="about-text">
-        NYC born and raised, I started dabbling with programming at a young age by making silly Flash games and clunky Lego Mindstorms.
-      </h2>
-    </color-section>
-    <color-section :color="colorAtIndex(3)">
-      <h2 class="about-text">
-        Many years later I'm still doing the same thing, just with MUCH cooler equiptment.
-      </h2>
-    </color-section>
-    <color-section v-for="(experience, index) in $page.experiences.edges" :color="colorAtIndex(4 + index)" :key="index">
-  </color-section>
+    <div class="sections">
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            Hi, I'm Miles
+          </h2>
+        </div>
+      </section>
+
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            I Like
+          </h2>
+          <div class="tag-list">
+            <div class="tag">Bits</div>
+            <div class="tag">Bots</div>
+            <div class="tag">Bio</div>
+            <div class="tag">Battlestar Galactica</div>
+          </div>
+        </div>
+      </section>
+
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            NYC born and raised, I started dabbling with programming at a young age by making silly Flash games and clunky Lego Mindstorms.
+          </h2>
+        </div>
+      </section>
+
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            Many years later I'm still doing the same thing, just with MUCH cooler equiptment.
+          </h2>
+        </div>
+      </section>
+
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            My beliefs are
+          </h2>
+          <div class="beliefs-list">
+            <div class="belief">
+              <h3 class="belief-header">
+                Belief Header
+              </h3>
+              <p class="belief-text">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div class="belief">
+              <h3 class="belief-header">
+                Belief Header
+              </h3>
+              <p class="belief-text">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div class="belief">
+              <h3 class="belief-header">
+                Belief Header
+              </h3>
+              <p class="belief-text">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div class="belief">
+              <h3 class="belief-header">
+                Belief Header
+              </h3>
+              <p class="belief-text">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="color-section">
+        <div class="section-content">
+          <h2 class="about-text">
+            Here's what I've worked on so far
+          </h2>
+        </div>
+      </section>
+
+      <section class="color-section" v-for="(experience, index) in $page.experiences.edges" :key="index">
+      </section>
+    </div>
   </Layout>
 </template>
 
@@ -50,12 +117,10 @@
 
 <script>
 import Logo from '~/components/Logo'
-import ColorSection from '~/components/ColorSection'
 
 export default {
   components: {
-    Logo,
-    ColorSection
+    Logo
   },
   data () {
     return {
@@ -85,7 +150,7 @@ export default {
       this.effectsToView.splice(randomEffectIndex, 1)
     },
     updateScrollProgressTarget () {
-      this.scrollProgressTarget = Math.max(1 - window.pageYOffset / window.innerHeight, 0)
+      this.scrollProgressTarget = Math.max(1 - window.pageYOffset / window.innerHeight / 1.5, 0)
     },
     updateScrollProgress () {
       this.scrollProgress += (this.scrollProgressTarget - this.scrollProgress) * this.scrollEase
@@ -121,18 +186,70 @@ export default {
   transform: translate(-50%, -50%);
 }
 
+.color-section {
+  min-height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .section-content {
+    max-width: 900px;
+    padding: 80px $spacing-l;
+    z-index: 1;
+  }
+
+  &:nth-child(3n+1) {
+    background-color: #FFFFFF;
+  }
+
+  &:nth-child(3n+2) {
+    background-color: #DEEEF9;
+  }
+
+  &:nth-child(3n+3) {
+    background-color: #DEF9EB;
+  }
+}
+
 .about-text {
   font-weight: 400;
   font-family: 'Averia Serif Libre';
   text-align: center;
 }
 
-.tag {
-  color: white;
-  padding: $spacing-xxxs $spacing-xxs;
-  margin-left: 0.5rem;
-  font-size: 1rem;
-  background-color: black;
+.tag-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  .tag {
+    color: white;
+    padding: $spacing-xxxs $spacing-xxs;
+    margin: 0.25rem;
+    background-color: black;
+  }
+}
+
+.beliefs-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  .belief {
+    width: 280px;
+    margin: $spacing-xxs;
+    padding: $spacing-xs $spacing-s;
+    border: 1px solid black;
+    flex-grow: 1;
+
+    .belief-header {
+      font-weight: 600;
+      margin-bottom: $spacing-xxxs;
+    }
+
+    .belief-text {
+      padding: 0;
+    }
+  }
 }
 
 .experience-section {
