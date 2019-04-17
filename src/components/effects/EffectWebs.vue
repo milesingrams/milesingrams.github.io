d394d699774f2306218ced6aea938399353541bc
Miles Ingram <milesingrams@gmail.com>
2019-04-17 01:22:05 -0400

    Fixed webs logo

--- src/components/effects/EffectWebs.vue
+++ src/components/effects/EffectWebs.vue
@@ -8,8 +8,8 @@
     </defs>
 
     <g class="poly-wrap" mask="url(#effect-mask)">
-      <polygon v-if="mergedOptions.poly" :points="polyPointString" :fill="mergedOptions.color" :opacity="mergedOptions.fillOpacity"></polygon>
-      <circle v-if="!mergedOptions.poly" cx="50" cy="50" :r="mergedOptions.radius" :fill="mergedOptions.color" :opacity="mergedOptions.fillOpacity"></circle>
+      <polygon v-if="mergedOptions.poly" :points="polyPointString" :fill="mergedOptions.color" :opacity="fillOpacity"></polygon>
+      <circle v-if="!mergedOptions.poly" cx="50" cy="50" :r="mergedOptions.radius" :fill="mergedOptions.color" :opacity="fillOpacity"></circle>
     </g>
   </svg>
 </template>
@@ -28,9 +28,9 @@ export default {
         poly: null,
         color: 'white',
         fillOpacity: 0.5,
-        minOpacity: 0.5,
-        maxOpacity: 1,
-        minStrokeWidth: 1,
+        minStrokeOpacity: 0.5,
+        maxStrokeOpacity: 1,
+        minStrokeWidth: 2,
         maxStrokeWidth: 8,
         numPaths: 60,
         numPointsPerPath: 10,
@@ -69,6 +69,13 @@ export default {
     generate () {
       this.paths = []
 
+      this.fillOpacity = 0
+      this.animationTimeline.add({
+        targets: this,
+        fillOpacity: this.mergedOptions.fillOpacity,
+        duration: this.mergedOptions.duration * 1000 / 2
+      })
+
       for (let i = 0; i < this.mergedOptions.numPaths; i++) {
         let dString = `M${this.randomPointPerimeter().join(',')}`
         for (let j = 0; j < this.mergedOptions.numPointsPerPath; j++) {
@@ -78,7 +85,7 @@ export default {
         let path = {
           dString,
           color: this.mergedOptions.color,
-          opacity: this.mergedOptions.minOpacity + Math.random() * (this.mergedOptions.maxOpacity - this.mergedOptions.minOpacity),
+          opacity: this.mergedOptions.minStrokeOpacity + Math.random() * (this.mergedOptions.maxStrokeOpacity - this.mergedOptions.minStrokeOpacity),
           strokeWidth: this.mergedOptions.maxStrokeWidth,
           dashArray: 400 * this.mergedOptions.numPointsPerPath,
           dashOffset: 0
@@ -98,7 +105,7 @@ export default {
   created () {
     this.animationTimeline = anime.timeline({
       autoplay: false,
-      easing: 'easeInQuad'
+      easing: 'easeOutQuad'
     })
     this.generate()
     this.updateSeek()
