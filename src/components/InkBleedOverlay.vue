dcaf625678794f2dc30e47d66a1d942e44a7a5ed
Miles Ingram <milesingrams@gmail.com>
2019-04-23 20:30:02 -0400

    Make ink bleed filter inline

--- src/components/InkBleedOverlay.vue
+++ src/components/InkBleedOverlay.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="ink-bleed-overlay" v-if="!finished">
-    <svg class="invisible" xmlns="http://www.w3.org/2000/svg">
-      <filter id="ink-filter">
-        <feTurbulence type="fractalNoise" baseFrequency="0.005" numOctaves="2" :seed="seed"/>
-        <feColorMatrix :values="colorMatrixValues" result="texture" />
-        <feComposite in="SourceGraphic" in2="texture" operator="in" />
-      </filter>
-    </svg>
-    <div class="filter-overlay" :style="{'background-color': this.mergedOptions.color}"></div>
-  </div>
+  <svg v-if="!finished" class="invisible" xmlns="http://www.w3.org/2000/svg">
+    <filter :id="inkFilterId">
+      <feTurbulence type="fractalNoise" baseFrequency="0.005" numOctaves="2" :seed="seed"/>
+      <feColorMatrix :values="colorMatrixValues" result="texture" />
+      <feComposite in="SourceGraphic" in2="texture" operator="in" />
+    </filter>
+  </svg>
 </template>
 
 <script>
@@ -31,11 +28,14 @@ export default {
     }
   },
   computed: {
+    inkFilterId () {
+      return `ink-filter-${this.seed}`
+    }
     mergedOptions () {
       return Object.assign({}, this.baseOptions, this.options)
     },
     colorMatrixValues () {
-      return `0 0 0 0 0, 0 0 0 0 0, 0 0 0 0 0, 0 0 0 ${-this.mergedOptions.intensity} ${(1 - this.progress) * this.mergedOptions.intensity}`
+      return `0 0 0 0 0, 0 0 0 0 0, 0 0 0 0 0, 0 0 0 ${-this.mergedOptions.intensity} ${this.progress * this.mergedOptions.intensity}`
     }
   },
   methods: {
@@ -45,7 +45,7 @@ export default {
         progress: 1,
         easing: 'easeInOutQuad',
         duration: this.mergedOptions.duration * 1000,
-      }, 0)
+      })
       this.animation.finished.then(() => {
         this.finished = true
       })
@@ -60,12 +60,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.filter-overlay {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  filter: url(#ink-filter);
-}
 </style>
